caa655cf46c755e1156998b31690f12a6a620e69
Maikel van Veen <maikeloo10@hotmail.com>
2019-04-09 11:17:13 +0200

    feat(client): add client ts

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 const gulp = require('gulp')
 const cleanCSS = require('gulp-clean-css')
+const uglify = require('gulp-uglify')
+const babel = require('gulp-babel')
+const plumber = require('gulp-plumber')
+const ts = require('gulp-typescript')
 
 gulp.task('minifyCss', () => {
     return gulp.src('./client/css/*.css')
@@ -7,14 +11,45 @@ gulp.task('minifyCss', () => {
         .pipe(gulp.dest('./server/public/css'))
 })
 
-gulp.task('moveJavaScript', () => {
-    return gulp.src('./client/scripts/*.js')
+gulp.task('minifyTs', () => {
+    return gulp.src('./client/scripts/*.ts')
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.error(error.message)
+                this.emit('end')
+            }
+        }))
+        .pipe(ts({
+            noImplicitAny: true,
+        }))
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
+        .pipe(gulp.dest('./server/public/scripts'))
+})
+
+gulp.task('minifyJs', () => {
+    return gulp.src('./client/scripts/*.ts')
+        .pipe(plumber({
+            errorHandler: function (error) {
+                console.error(error.message)
+                this.emit('end')
+            }
+        }))
+        .pipe(ts({
+            noImplicitAny: true,
+        }))
+        .pipe(babel({
+            presets: ['@babel/env']
+        }))
+        .pipe(uglify())
         .pipe(gulp.dest('./server/public/scripts'))
 })
 
 gulp.task('watchClient', () => {
     if (process.env.NODE_ENV !== 'production') {
         gulp.watch(['./client/css/*.css'], gulp.series('minifyCss'))
-        gulp.watch(['./client/scripts/*.js'], gulp.series('moveJavaScript'))
+        gulp.watch(['./client/scripts/*.ts'], gulp.series('minifyTs'))
     }
 })
