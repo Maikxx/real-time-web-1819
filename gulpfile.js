5af729514b3d0d132dde77840964ff70c285e125
Maikel van Veen <maikeloo10@hotmail.com>
2019-04-10 16:05:27 +0200

    feat(css): css -> sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 const gulp = require('gulp')
+const sass = require('gulp-sass')
 const cleanCSS = require('gulp-clean-css')
 
+sass.compiler = require('node-sass')
+
 gulp.task('minifyCss', () => {
-    return gulp.src('./client/css/*.css')
+    return gulp.src('./client/css/*.scss')
+        .pipe(sass())
         .pipe(cleanCSS())
         .pipe(gulp.dest('./server/public/css'))
 })
 
 gulp.task('watchClient', () => {
     if (process.env.NODE_ENV !== 'production') {
-        gulp.watch(['./client/css/*.css'], gulp.series('minifyCss'))
+        gulp.watch(['./client/css/*.scss'], gulp.series('minifyCss'))
     }
 })
