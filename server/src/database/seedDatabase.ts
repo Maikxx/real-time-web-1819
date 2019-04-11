3072e316fba3ed20b822e7a4523b626999ab1760
Maikel van Veen <maikeloo10@hotmail.com>
2019-04-11 18:42:16 +0200

    refactor(seeders): split code

--- server/src/database/seedDatabase.ts
+++ server/src/database/seedDatabase.ts
@@ -3,6 +3,10 @@ import { addCryptoCurrency } from './crypto_currencies/addCryptoCurrency'
 require('dotenv').config()
 
 export async function seedDatabase() {
+    await seedCryptoCurrencies()
+}
+
+async function seedCryptoCurrencies() {
     const cryptoCompareCoinListUrl = `https://min-api.cryptocompare.com/data/all/coinlist?api_key=${process.env.CRYPTO_COMPARE_KEY}`
 
     const response: Response = await fetch(cryptoCompareCoinListUrl)
