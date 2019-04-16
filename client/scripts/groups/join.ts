0b78ca7262039b5aeac7a798db74dd9dc99c86c5
Maikel van Veen <maikeloo10@hotmail.com>
2019-04-16 18:43:20 +0200

    refactor(join): make join code more readable

--- client/scripts/groups/join.ts
+++ client/scripts/groups/join.ts
@@ -64,33 +64,7 @@ import { GroupQueryResult, Group } from '../types/Group'
                         const data: GroupQueryResult = await response.json()
 
                         if (data && !data.error) {
-                            const { group } = data
-
-                            const dynamicHTML = `
-                                <div class="Field">
-                                    <span class="Field__title">
-                                        Group currency
-                                    </span>
-                                    <div class="Field__content">
-                                        <span>${group.crypto_currency}</span>
-                                    </div>
-                                </div>
-
-                                <div class="Field">
-                                    <span class="Field__title">
-                                        Users in this group
-                                    </span>
-                                    <div class="Field__content">
-                                        ${group.participants && group.participants.length > 0
-                                            // tslint:disable-next-line:ter-max-len
-                                            ? `<ul class="Row Row--wrap">${group.participants.map(participant => `<li>${participant.username}</li>`)}</ul>`
-                                            : 'No participants found for this group.'
-                                        }
-                                    </div>
-                                </div>
-                            `
-
-                            JS_DYNAMIC_HTML_HOOK.innerHTML = dynamicHTML
+                            JS_DYNAMIC_HTML_HOOK.innerHTML = getDynamicGroupMarkup(data.group)
                         } else {
                             throw new Error(data.error as string)
                         }
@@ -104,5 +78,36 @@ import { GroupQueryResult, Group } from '../types/Group'
                 throw new Error('One of the required elements could not be found!')
             }
         }
+    }
+
+    function getDynamicGroupMarkup(group: Group) {
+        return `
+        <div class="Field">
+            <span class="Field__title">
+                Group currency
+            </span>
+            <div class="Field__content">
+                <span>${group.crypto_currency}</span>
+            </div>
+        </div>
+
+        <div class="Field">
+            <span class="Field__title">
+                Users in this group
+            </span>
+            <div class="Field__content">
+                ${group.participants && group.participants.length > 0
+                    ? (
+                        `<ul class="Row Row--wrap">
+                            ${group.participants.map(participant => (
+                                `<li>${participant.username}</li>`
+                            ))}
+                        </ul>`
+                    )
+                    : 'No participants found for this group.'
+                }
+            </div>
+        </div>
+    `
     }
 })()
