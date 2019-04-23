28dd73df9ae6468a29fa1d30d14f914de406f6c9
Maikel van Veen <maikeloo10@hotmail.com>
2019-04-23 11:40:05 +0200

    fix(events): setup max listeners

--- server/src/www/events.ts
+++ server/src/www/events.ts
@@ -3,3 +3,7 @@ import EventEmitter from 'events'
 export const groupCreationEmitter = new EventEmitter()
 export const groupParticipantCreationEmitter = new EventEmitter()
 export const groupScoreChanged = new EventEmitter()
+
+groupCreationEmitter.setMaxListeners(1000)
+groupParticipantCreationEmitter.setMaxListeners(1000)
+groupScoreChanged.setMaxListeners(1000)
