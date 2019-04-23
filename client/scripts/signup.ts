589c1b0f55e61251e62b022f5f3b00ce92f57d1d
Maikel van Veen <maikeloo10@hotmail.com>
2019-04-23 19:30:30 +0200

    feat(signup): add client-side validation

--- client/scripts/signup.ts
+++ client/scripts/signup.ts
@@ -10,10 +10,14 @@ import { State } from './State'
 
         const emailInput = document.querySelector('[name="email"]')
         const passwordInput = document.querySelector('[name="password"]')
+        const repeatPasswordInput = document.querySelector('[name="repeat-password"]')
+        const nicknameInput = document.querySelector('[name="name"]')
 
-        if (emailInput && passwordInput) {
+        if (emailInput && passwordInput && repeatPasswordInput && nicknameInput) {
             emailInput.addEventListener('keyup', onInput('email'))
             passwordInput.addEventListener('keyup', onInput('password'))
+            repeatPasswordInput.addEventListener('keyup', onInput('repeat-password'))
+            nicknameInput.addEventListener('keyup', onInput('name'))
         } else {
             toast.error('Not all required input fields could be found to validate input.')
         }
@@ -29,11 +33,18 @@ import { State } from './State'
         function validate() {
             const email = state.get('email')
             const password = state.get('password')
+            const repeatPassword = state.get('repeat-password')
+            const nickname = state.get('name')
 
             if (!email
                 || email.length === 0
                 || !password
                 || password.length === 0
+                || !repeatPassword
+                || repeatPassword.length === 0
+                || !nickname
+                || nickname.length === 0
+                || password !== repeatPassword
             ) {
                 return disableActionButton()
             }
